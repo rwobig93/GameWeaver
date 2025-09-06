411f0f21d7bf1092023535c71da91b79493ba6cf
Rick Wobig <rick@wobigtech.net>
2025-09-06 15:07:49 -0500

    Finish owned hosts and gameserver views on the index page

--- src/server/GameWeaver/Components/GameServer/GameServerWidget.razor.cs
+++ src/server/GameWeaver/Components/GameServer/GameServerWidget.razor.cs
@@ -7,11 +7,6 @@ namespace GameWeaver.Components.GameServer;
 
 public partial class GameServerWidget : ComponentBase
 {
-    private string _cssBorderBase = "rounded-lg justify-center align-center mud-text-align-center";
-    private string _cssBorderStatus = " border-status-default";
-    private string _cssTextStatus = "";
-
-    private GameSlim _game = new() {Id = Guid.Empty, FriendlyName = "Unknown"};
     [Parameter] public GameServerSlim GameServer { get; set; } = new();
     [Parameter] public int WidthPx { get; set; } = 400;
     [Parameter] public int HeightPx { get; set; } = 100;
@@ -19,9 +14,14 @@ public partial class GameServerWidget : ComponentBase
 
     [Inject] public IGameService GameService { get; init; } = null!;
 
+    private GameSlim _game = new() {Id = Guid.Empty, FriendlyName = "Unknown"};
 
     private string Width => $"{WidthPx}px";
     private string Height => $"{HeightPx}px";
+    private string _cssBorderBase = "rounded-lg justify-center align-center mud-text-align-center";
+    private string _cssBorderStatus = " border-status-default";
+    private string _cssTextStatus = "";
+
 
     protected override async Task OnAfterRenderAsync(bool firstRender)
     {
